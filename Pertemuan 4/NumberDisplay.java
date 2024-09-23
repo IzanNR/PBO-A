9a4ccd85c5ead939caa996c40b63e570e09bdb9d
IzanNR <162288339+IzanNR@users.noreply.github.com>
2024-09-23 19:35:47 +0700

    Create NumberDisplay.java

---
+++ Pertemuan 4/NumberDisplay.java
@@ -0,0 +1,30 @@
+public class NumberDisplay {
+    private int limit, value;
+
+    public NumberDisplay(int rollOverLimit) {
+        limit = rollOverLimit;
+        value = 0;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDisplayValue() {
+        if(value < 10) {
+            return "0" + value;
+        } else {
+            return "" + value;
+        }
+    }
+
+    public void setValue(int replaceValue) {
+        if((replaceValue >= 0) && (replaceValue < limit)) {
+            value = replaceValue;
+        }
+    }
+
+    public void increment() {
+        value = (value + 1) % limit;
+    }
+}
